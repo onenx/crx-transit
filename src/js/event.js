d602c40447f1f40965ea37198ac4ed2f5ea91b29
greatghoul <greatghoul@gmail.com>
2018-01-23 21:00:59 +0800

    解决百度翻译失效的问题

--- src/js/event.js
+++ src/js/event.js
@@ -24,28 +24,6 @@ function currentText(text) {
   }
 }
 
-window.loadPage = function (url, callback) {
-  chrome.tabs.executeScript(null, { code: 'document.body.setAttribute("goo", "time");' });
-  const frame = document.createElement('iframe');
-  frame.src = url;
-  document.body.appendChild(frame);
-
-  frame.addEventListener('load',function() {
-    const token = cuid();
-    frame.contentWindow.postMessage({ source: 'transit', token: token }, '*');
-
-    window.addEventListener('message', function (e) {
-      console.log('message from sub frame', e);
-
-      const message = e.data;
-      if (message.source == 'baidu_bridge' && message.token == token) {
-        callback(message.result);
-        window.removeEventListener.removeListener('message', arguments.callee);
-      }
-    });
-  });
-};
-
 // Translate text and send result back
 // 
 // TODO: Cache translated result to speed up querying.
--- src/js/translators/baidu_translator.js
+++ src/js/translators/baidu_translator.js
@@ -5,53 +5,63 @@
  */
 
 import $ from 'jquery';
-import utils from '../lib/utils';
 import _ from 'lodash';
-import settings from '../config/settings';
-import md5 from 'md5';
 
 const API_URL = 'http://fanyi.baidu.com/basetrans';
+const USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1';
+
+// 修改百度翻译 API 请求，将 User Agent 修改为移动设备
+chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
+  let header = null;
+
+  header = _.find(details.requestHeaders, { name: 'User-Agent' });
+  header.value = USER_AGENT;
+
+  header = _.find(details.requestHeaders, { name: 'Origin' });
+  header.value = 'http://fanyi.baidu.com';
+
+  return { requestHeaders: details.requestHeaders };
+}, { urls: [API_URL] }, ['blocking', 'requestHeaders']);
 
 function formatResult(result) {
   if (!result) return null;
 
-  var response = {};
+  const response = {};
 
-  if (!_.isEmpty(result.dict_result)) {
+  if (_.has(result, 'dict.symbols[0].parts')) {
     try {
-      var symbols = result.dict_result.simple_means.symbols[0];
-
-      if (symbols.ph_am) {
-        response.phonetic = `[${symbols.ph_am}]`;
+      const phonetic = _.get(result, 'dict.symbols[0].ph_am');
+      if (phonetic) {
+        response.phonetic = `[${phonetic}]`;
       }
 
-      response.translation = symbols.parts.map(function (part) {
-        return `${part.part} ${part.means.join('; ')}`;
-      }).join('<br/>');
+      const parts = _.get(result, 'dict.symbols[0].parts', []);
+      response.translation = _.map(parts, (part) => `${part.part} ${part.means.join(';')}`).join('<br/>');
     } catch (e) {
       return null;
     }
   } else {
-    console.log(result);
-    var trans_result = result.trans_result.data[0];
-    if (trans_result.src == trans_result.dst) return null;
-    response.translation = trans_result.dst;
+    const trans_result = _.get(result, 'trans[0]', {});
+    if (trans_result.src == trans_result.dst) {
+      return null;
+    } else {
+      response.translation = trans_result.dst;
+    }
   }
 
   return response;
 }
 
-function generateSign (text, salt) {
-  const source = `${settings.BAIDU_APP_ID}${text}${salt}${settings.BAIDU_APP_SECRET}`;
-  return md5(source);
-}
-
 function requestText(text, callback) {
+  let headers = new Headers();
+  headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
   fetch(API_URL, {
     method: 'POST',
-    body: $.param({ from: 'en', to: 'en', 'query': text }),
-    mode: 'cors',
-  }).then(response => console.log(response))
+    body: $.param({ from: 'en', to: 'zh', 'query': text }),
+    headers: headers
+  }).then(response => response.json())
+    .then(result => callback(formatResult(result)))
     .catch(() => callback(null));
 }
 
